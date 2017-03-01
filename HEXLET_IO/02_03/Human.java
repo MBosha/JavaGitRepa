34a42dda429f3acfcac4ed6680885fbf46a1c715
MBosha <boshyn@gmail.com>
2017-03-01 12:41:25 +0300

    Введение в Java / Модуль 2. Урок 3. Работа с экземплярами в Java.

---
+++ HEXLET_IO/02_03/Human.java
@@ -0,0 +1,8 @@
+class Human {
+
+  String name;
+  int age;
+  String surname;
+  boolean sex;
+
+}
---
+++ HEXLET_IO/02_03/Main.java
@@ -0,0 +1,19 @@
+class Main {
+
+  public static void main (String... args) {
+
+    Human slava = null;
+    slava = new Human ();
+    System.out.println (slava.age);
+
+    slava.age = 12;
+    System.out.println (slava.age);
+
+    Human max = new Human ();
+    max.age = 13;
+    max.name = "Max";
+    System.out.println (max.age);
+    System.out.println (max.name);
+
+  }
+}
