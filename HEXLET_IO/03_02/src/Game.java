66bb64335877e08629204ea7e7ec6c39c1f25b26
MBosha <boshyn@gmail.com>
2017-03-03 13:36:14 +0300

    Дополнение

---
+++ HEXLET_IO/03_02/src/Game.java
@@ -0,0 +1,12 @@
+class Game {
+    int levels = 3;
+
+    public static void printGameName() {
+        System.out.println("Star Wars!");
+    }
+
+    public void printLevelCount() {
+        System.out.println("Levels count: " + this.levels);
+    }
+
+}
